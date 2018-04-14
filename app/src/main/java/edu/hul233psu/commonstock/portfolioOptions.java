40da8f395cd241d87aea90bd0fda934407d5e24a
aub309 <aub309@psu.edu>
2018-04-14 19:58:47 -0400

    Added some stuff to show the values that were calculated

--- app/src/main/java/edu/hul233psu/commonstock/portfolioOptions.java
+++ app/src/main/java/edu/hul233psu/commonstock/portfolioOptions.java
@@ -27,7 +27,7 @@ public class portfolioOptions extends AppCompatPreferenceActivity{
 
         continuebutton=findViewById(R.id.button4);
 
-        continuebutton.setOnClickListener(ContinueListener); //Wires the button to the UI Listener
+        continuebutton.setOnClickListener(ContinueListener); //Wires the button to the UI Listene
     }
 
     public void onCheckboxClicked(View view) {
