76b7dff2f1e9d1948744b585204731d9675daf96
aub309 <aub309@psu.edu>
2018-04-14 19:20:26 -0400

    This is what I now have. I don't know why but for some reason it is doing something with a ListView when I click the PortfolioOptions. However I did add the calculations now. Maybe you can find what's wrong?

--- app/src/main/java/edu/hul233psu/commonstock/Stock_Results.java
+++ app/src/main/java/edu/hul233psu/commonstock/Stock_Results.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Stock_Results extends AppCompatActivity {
@@ -23,6 +24,17 @@ public class Stock_Results extends AppCompatActivity {
     Button emailresultbutton;
     Button done;
 
+    TextView valueup;
+    TextView valuedown;
+    TextView valuerisk;
+    TextView valuereturn;
+
+
+    public static float valueifup;
+    public static float valueifdown;
+    public static float prisk;
+    public static float preturn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +42,20 @@ public class Stock_Results extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        emailresultbutton= findViewById(R.id.emailresultsbutton);
+
+        calculateresults();
+
+        emailresultbutton = findViewById(R.id.emailresultsbutton);
         done = findViewById(R.id.buttondone);
+        valueup = findViewById(R.id.Valueup);
+        valuedown = findViewById(R.id.ValueDown);
+        valuerisk = findViewById(R.id.ValueRisk);
+        valuereturn = findViewById(R.id.ValueReturn);
+
+        valueup.setText(Float.toString(valueifup));
+        valuedown.setText(Float.toString(valueifdown));
+        valuerisk.setText(Float.toString(prisk));
+        valuereturn.setText(Float.toString(preturn));
 
         emailresultbutton.setOnClickListener(emailButtonListener); //Wires the button to the UI Listener
         done.setOnClickListener(doneButtonListener); //Wires the button to the UI Listener
@@ -55,7 +79,66 @@ public class Stock_Results extends AppCompatActivity {
         }
     };
 
-    public void displaySave(final View view){
+    private void calculateresults() {
+        float stocksup;
+        float stocksdown;
+        float bonds;
+        float futurecontractsup;
+        float futurecontractsdown;
+        float calloptionsup;
+        float calloptionsdown;
+        float putoptionsup;
+        float putoptionsdown;
+
+        float sinitvalue = UserInput.stockinitvalue;
+        float binitvalue = UserInput.bondinitvalue;
+        float finitvalue = UserInput.forwardcinitvalue;
+        float cinitvalue = UserInput.callinitvalue;
+        float pinitvalue = UserInput.putinitvalue;
+
+        float snumber = UserInput.stocknumber;
+        float bnumber = UserInput.bondnumber;
+        float fnumber = UserInput.forwardcnumber;
+        float cnumber = UserInput.callnumber;
+        float pnumber = UserInput.putnumber;
+
+        float sfuturelow = UserInput.stockfuturelow;
+        float ffuturelow = UserInput.forwardcfuturelow;
+        float cfuturelow = UserInput.callfuturelow;
+        float pfuturelow = UserInput.putfuturelow;
+
+        float sfuturehigh = UserInput.stockfuturehigh;
+        float bfuture = UserInput.bondfuture;
+        float ffuturehigh = UserInput.forwardcfuturehigh;
+        float cfuturehigh = UserInput.callfuturehigh;
+        float pfuturehigh = UserInput.putfuturehigh;
+        float percentage = UserInput.percent;
+        float riskpart1;
+        float riskpart2;
+        float percentup = percentage / 100;
+        float percentdown = 1 - percentup;
+
+
+        stocksup = ((sfuturehigh * snumber) - (sinitvalue * snumber)) / (sinitvalue * snumber);
+        stocksdown = ((sfuturelow * snumber) - (sinitvalue * snumber)) / (sinitvalue * snumber);
+        bonds = ((bfuture * bnumber) - (binitvalue * bnumber)) / (binitvalue * bnumber);
+        futurecontractsup = ((ffuturehigh * fnumber) - (finitvalue * fnumber)) / (finitvalue * fnumber);
+        futurecontractsdown = ((ffuturelow * fnumber) - (finitvalue * fnumber)) / (finitvalue * fnumber);
+        calloptionsup = ((cfuturehigh * cnumber) - (cinitvalue * cnumber)) / (cinitvalue * cnumber);
+        calloptionsdown = ((cfuturelow * cnumber) - (cinitvalue * cnumber)) / (cinitvalue * cnumber);
+        putoptionsup = ((pfuturehigh * pnumber) - (pinitvalue * pnumber)) / (pinitvalue * pnumber);
+        putoptionsdown = ((pfuturelow * pnumber) - (pinitvalue * pnumber)) / (pinitvalue * pnumber);
+
+        Stock_Results.valueifup = stocksup + bonds + futurecontractsup + calloptionsup + putoptionsup;
+        Stock_Results.valueifdown = stocksdown + bonds + futurecontractsdown + calloptionsdown + putoptionsdown;
+        Stock_Results.preturn = (Stock_Results.valueifup * percentup) + (Stock_Results.valueifdown * percentdown);
+        riskpart1 = (percentup * ((Stock_Results.valueifup - Stock_Results.preturn) * (Stock_Results.valueifup - Stock_Results.preturn)));
+        riskpart2 = (percentdown * ((Stock_Results.valueifdown - Stock_Results.preturn) * (Stock_Results.valueifdown - Stock_Results.preturn)));
+        Stock_Results.prisk = (float) Math.sqrt(riskpart1 + riskpart2);
+
+    }
+
+    public void displaySave(final View view) {
         //Code that subclasses AlertDialog
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setMessage("Save the data bafore finish?"); //Code to display the dialog
@@ -69,7 +152,7 @@ public class Stock_Results extends AppCompatActivity {
 
                 });
 
-        alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
             @Override //Code for the other buttons
             public void onClick(DialogInterface dialog, int which) {
                 Intent intent = new Intent(Stock_Results.this, MainActivity.class);
@@ -99,7 +182,7 @@ public class Stock_Results extends AppCompatActivity {
                 saveTitle = input.getText().toString();
                 Intent intent = new Intent(Stock_Results.this, MainActivity.class);
                 startActivity(intent);
-                Toast.makeText(Stock_Results.this,"Data Saved",Toast.LENGTH_LONG).show();
+                Toast.makeText(Stock_Results.this, "Data Saved", Toast.LENGTH_LONG).show();
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -111,7 +194,5 @@ public class Stock_Results extends AppCompatActivity {
 
         builder.show();
     }
-
-
 
 }
