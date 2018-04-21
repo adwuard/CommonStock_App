27b372ad03bf617fdae4ded95ba543f95f60d634
aub309 <aub309@psu.edu>
2018-04-21 14:24:22 -0400

    Added some stuff for the CurrentStockRates

--- app/src/main/java/edu/hul233psu/commonstock/CurrentStockRates.java
+++ app/src/main/java/edu/hul233psu/commonstock/CurrentStockRates.java
@@ -6,8 +6,33 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.acl.LastOwnerException;
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONArray;
 
 public class CurrentStockRates extends AppCompatActivity {
+
+    Button equityButton;
+    EditText eqname;
+    String EQname = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +41,11 @@ public class CurrentStockRates extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        equityButton = findViewById(R.id.EquityButton);
+        eqname = findViewById(R.id.EquityEditText);
 
     }
+
+
 
 }
